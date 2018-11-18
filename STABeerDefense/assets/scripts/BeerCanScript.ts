206ac41fa07dcc953b8e1f6402d2eee5329533df
AnEmperor <theotherside03@hotmail.com>
2018-11-18 08:58:34 -0600

    - Updated Beer Can to pass through click point and continue off screen if it doesn't impact an enemy

--- STABeerDefense/assets/scripts/BeerCanScript.ts
+++ STABeerDefense/assets/scripts/BeerCanScript.ts
@@ -53,36 +53,51 @@ rotSpeed: number = 15;
         var parentHeight = this.node.getParent().height;
 
         // offset value because we're coming up short
+        // required for jump functionality
         jumpHeight += 200;
 
         // converting world space of mouse position to node space of beer can position
         var newTgtLoc = this.node.getParent().convertToNodeSpaceAR(tgtLocation);
  
+        // calculate full trajectory through click point
+        newTgtLoc = this.CalculateTrajectory(newTgtLoc)
+
         // normalizing velocity based on distance to travel
-        var timeToTarget = this.maxTravelDuration * ((jumpHeight-200)/parentHeight);
+        // required for jump functionality
+        //var timeToTarget = this.maxTravelDuration * ((jumpHeight-200)/parentHeight);
 
         // shift target x position so that height of jump passes through target x position
+        // required for jump functionality
         var dist = newTgtLoc.x - this.node.position.x;
 
         // define movement action parameters
         // var action = cc.jumpTo(timeToTarget, newTgtLoc.x+dist, -(this.node.getParent().height), jumpHeight, 1);//.easing(cc.easeInOut(1));
-        var action = cc.moveTo(timeToTarget, newTgtLoc);//.easing(cc.easeInOut(1));
+        var action = cc.moveTo(this.maxTravelDuration, newTgtLoc);//.easing(cc.easeInOut(1));
 
         // execute movement
         this.node.runAction(action);
 
+        // initialize variables that help determine when can has reached its destination
         this.destination = newTgtLoc;
         this.initialized = true;
     }
 
-    CalculateTrajectory(tgtLocation: cc.Vec2)
+    CalculateTrajectory = function(tgtLocation: cc.Vec2)
     {
+        var newTgtLoc = new cc.Vec2;
+
         // m = y2-y1 / x2-x1
         var m = (this.node.position.y - tgtLocation.y) / (this.node.position.x - tgtLocation.x);
-        console.log("Slope: " + m);
-//y = mx + b
+        // console.log("Slope: " + m);
+
         // b = y / mx
         var b = tgtLocation.y / (m * tgtLocation.x);
-        console.log("Intercept: " + b);
+        // console.log("Intercept: " + b);
+
+        // x = y-b/m
+        newTgtLoc.x = (this.node.getParent().height - b)/m;
+        newTgtLoc.y = this.node.getParent().height;
+
+        return newTgtLoc;
     }
 }
